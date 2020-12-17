f16a321765987500a54c6c493b0cbe234538f349
sbsamboat9 <sbsamboat9@icloud.com>
2020-12-17 14:18:54 -0500

    commiting in view and config files

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
-  before_action :admin_user
+  before_action :admin_user, except: [:show]
 
   # GET /categories
   # GET /categories.json
@@ -11,6 +11,11 @@ class CategoriesController < ApplicationController
   # GET /categories/1
   # GET /categories/1.json
   def show
+    @articles = @category.articles
+    respond_to do |format|
+    format.html { render "articles/index" }
+    format.json { render :show, status: :created, location: @articles }
+    end
   end
 
   # GET /categories/new
